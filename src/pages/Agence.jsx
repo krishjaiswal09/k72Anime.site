be8c8795388a9ca9c3abfe364a686d48a087396e
Krish Jaiswal <117284024+krishjaiswal09@users.noreply.github.com>
2025-09-06 23:08:22 +0530

    Coded agence page

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
+import gsap from "gsap";
+import React, { useRef } from "react";
 
 const Agence = () => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const imageDivRef = useRef(null);
+
+  useGSAP(function () {
+    gsap.to(imageDivRef.current, {
+      scrollTrigger: {
+        trigger: imageDivRef.current,
+        markers: true,
+        start: "top 33.9%",
+        end: "top -90%",
+        scrub: true,
+        pin: true,
+      },
+    });
+  });
+
   return (
     <div>
-      Agence
-    </div>
-  )
-}
+      <div className="section1">
+        <div
+          ref={imageDivRef}
+          className="absolute h-[20vw] w-[15vw] top-80 left-[32vw] rounded-2xl overflow-hidden"
+        >
+          <img
+            src="https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg"
+            alt=""
+            className="h-full object-cover w-full object-cover"
+          />
+        </div>
 
-export default Agence
+        <div className=" relative font-[font2]">
+          <div className="mt-[56vh]">
+            <h1 className="text-[18vw] text-center uppercase leading-[18vw]">
+              {" "}
+              Soixan7e Douze
+            </h1>
+          </div>
+          <div className="pl-[43%] mt-15">
+            <p className="text-6xl">
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Notre
+              curiosité nourrit notre créativité. On reste humbles et on dit non
+              aux gros egos, même le vôtre. Une marque est vivante. Elle a des
+              valeurs, une personnalité, une histoire. Si on oublie ça, on peut
+              faire de bons chiffres à court terme, mais on la tue à long terme.
+              C’est pour ça qu’on s’engage à donner de la perspective, pour
+              bâtir des marques influentes.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="section2 h-screen"></div>
+    </div>
+  );
+};
+
+export default Agence;
